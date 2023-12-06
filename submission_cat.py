7edd614b900140f0e55382c1ee11ab0e9602d0a9
Ameya1337 <ameyask99@gmail.com>
2023-12-06 12:50:49 +0100

    "submission_cat updated"

--- submission_cat.py
+++ submission_cat.py
@@ -23,19 +23,12 @@ numerical_cols = ["latitude", "longitude"]
 for feature in categorical_cols:
     data[feature] = data[feature].astype(str)
 
-cols_to_drop = [
-    "counter_id",
-    "site_id",
-    "date",
-    "counter_installation_date",
-    "coordinates",
-]
+cols_to_drop_train = ["counter_id", "site_id", "date", "counter_installation_date"]
 
-data = data.drop(cols_to_drop, axis=1)
-data = data.drop("bike_count")
-X = data.drop("log_bike_count")
+data = data.drop(cols_to_drop_train, axis=1)
+data = data.drop("bike_count", axis=1)
+X = data.drop("log_bike_count", axis=1)
 y = data["log_bike_count"]
-
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
@@ -67,7 +60,10 @@ test_pool = Pool(
 
 
 best_cat_reg = CatBoostRegressor(
-    cat_features=categorical_cols, verbose=True, **best_params
+    cat_features=categorical_cols,
+    verbose=True,
+    **best_params,
+    early_stopping_rounds=10,
 )
 
 best_cat_reg.fit(train_pool, eval_set=test_pool)
@@ -78,8 +74,15 @@ test_data["date"] = pd.to_datetime(test_data["date"])
 test_data["day_of_week"] = test_data["date"].dt.dayofweek
 test_data["month"] = test_data["date"].dt.month
 test_data["hour"] = test_data["date"].dt.hour
+cols_to_drop_test = [
+    "counter_id",
+    "site_id",
+    "date",
+    "counter_installation_date",
+    "coordinates",
+]
 
-test_data = test_data.drop(cols_to_drop, axis=1)
+test_data = test_data.drop(cols_to_drop_test, axis=1)
 
 predictions = best_cat_reg.predict(test_data)
 predictions_df = pd.DataFrame({"log_bike_count": predictions})
