24a43310583ea8ca297d8139380baf14670a9643
Ameya1337 <ameyask99@gmail.com>
2023-12-08 21:38:14 +0100

    "submission_ts_xgb"

--- submission_ts_xgb.py
+++ submission_ts_xgb.py
@@ -1,32 +1,39 @@
 import pandas as pd
 import xgboost as xgb
 
+# Load the data
 data = pd.read_parquet("/kaggle/input/mdsb-2023/train.parquet")
 test_data = pd.read_parquet("/kaggle/input/mdsb-2023/final_test.parquet")
+
+# Convert 'date' to datetime and set as index
 data["date"] = pd.to_datetime(data["date"])
 data = data.set_index("date")
 test_data["date"] = pd.to_datetime(test_data["date"])
 test_data = test_data.set_index("date")
+
+# Select specific counter name
 selected_counter_name = "Totem 73 boulevard de Sébastopol S-N"
 train = data[data["counter_name"] == selected_counter_name]
-test = test_data[["counter_name"]]
-test = test[test["counter_name"] == selected_counter_name]
+test = test_data[test_data["counter_name"] == selected_counter_name]
 
 
-def create_features(train):
+def create_features(df):
     """
     Create time series features based on timeseries index
     """
-    train["hour"] = train.index.hour
-    train["dayofweek"] = train.index.dayofweek
-    train["quarter"] = train.index.quarter
-    train["month"] = train.index.month
-    train["dayofyear"] = train.index.dayofyear
-    return train
+    df["hour"] = df.index.hour
+    df["dayofweek"] = df.index.dayofweek
+    df["quarter"] = df.index.quarter
+    df["month"] = df.index.month
+    df["dayofyear"] = df.index.dayofyear
+    return df
 
 
+# Create features for both training and test datasets
 train = create_features(train)
 test = create_features(test)
+
+# XGBoost parameters
 best_params = {
     "colsample_bytree": 0.9,
     "learning_rate": 0.1,
@@ -35,6 +42,8 @@ best_params = {
     "n_estimators": 500,
     "subsample": 0.9,
 }
+
+# Initialize XGBoost Regressor
 best_reg = xgb.XGBRegressor(
     tree_method="hist",
     n_estimators=best_params["n_estimators"],
@@ -46,20 +55,27 @@ best_reg = xgb.XGBRegressor(
     enable_categorical=True,
 )
 
+# Define features and target
 features = ["counter_name", "hour", "dayofweek", "quarter", "month", "dayofyear"]
 target = ["log_bike_count"]
 
+# Prepare training data
 X_train = train[features]
 y_train = train[target]
 
+# Train the model
 best_reg.fit(
     X_train,
     y_train,
     verbose=10,
 )
 
+# Prepare test data
 X_test = test[features]
 
+# Make predictions
 predictions = best_reg.predict(X_test)
-predictions_df = pd.DataFrame({"log_bike_count": predictions})
+
+# Create a DataFrame for predictions with the same index as test_data
+predictions_df = pd.DataFrame({"log_bike_count": predictions}, index=test.index)
 predictions_df.to_csv("submissions.csv", index=True, index_label="Id")
